e81f819352e9cf7df7935203a14f6fd887e992b4
henryy07 <adam.harmasz@o2.pl>
2019-02-06 13:50:28 +0100

    added check_encoding function which uses chardet package to detect encoding of the file we want to use

--- main.py
+++ main.py
@@ -3,13 +3,22 @@ import pycountry
 import csv
 from datetime import datetime
 from math import ceil
+import chardet
+
+
+def check_encoding(file_path):
+    """Function that defines encoding of the file"""
+    raw_data = open (file_path, 'rb').read()
+    result = chardet.detect(raw_data)
+    encoding = result['encoding']
+    return encoding
 
 
 def get_country_and_change_date_format(my_input):
     """
     Function defining country from the given state name and changing date
     format to the YYYY-MM-DD format
-    function should return three letter country code
+    function should return generator of a new OrederedDict
     """
     for state in my_input:
         try:
@@ -21,7 +30,8 @@ def get_country_and_change_date_format(my_input):
             state['date'] = datetime.strptime(
                 state['date'],
                 '%m/%d/%Y').strftime('%Y-%m-%d')
-            state['ctr'] = ceil(float(state['impressions']) * (float(state['ctr'][0:3]) * 0.01))
+            state['ctr'] = ceil(float(state['impressions']) *
+                                (float(state['ctr'][0:3]) * 0.01))
             yield state
 
         # if state name is not included in pycountry library exception will be
@@ -33,17 +43,26 @@ def get_country_and_change_date_format(my_input):
             state['date'] = datetime.strptime(
                 state['date'],
                 '%m/%d/%Y').strftime('%Y-%m-%d')
-            state['ctr'] = ceil(float(state['impressions']) * (float(state['ctr'][0:3]) * 0.01))
+            state['ctr'] = ceil(float(state['impressions']) *
+                                (float(state['ctr'][0:3]) * 0.01))
             state['impressions'] = float(state['impressions'])
             yield state
 
 
-with open('new_sample', 'r') as f:
-    csv_reader = csv.DictReader(f)
-    new_list = get_country_and_change_date_format(csv_reader)
-    df_2 = pd.DataFrame(data=new_list)
-    df_2['impressions'] = df_2['impressions'].apply(lambda x: int(x))
-    date_groups = df_2.groupby(['date', 'state']).sum().reset_index()
-    date_groups.set_index('date')
-    print(date_groups)
-    date_groups.to_csv('new_file.csv', encoding='UTF-8', index=False)
+def main(file_path):
+    """Main function of the application"""
+    encode_type = 'UTF-8'
+    if check_encoding(file_path) == 'UTF-16':
+        encode_type = 'UTF-16'
+
+    with open(file_path, 'r', encoding=encode_type) as f:
+        csv_reader = csv.DictReader(f)
+        new_list = get_country_and_change_date_format(csv_reader)
+        df_2 = pd.DataFrame(data=new_list)
+        df_2['impressions'] = df_2['impressions'].apply(lambda x: int(x))
+        date_groups = df_2.groupby(['date', 'state']).sum().reset_index()
+        date_groups.set_index('date')
+        date_groups.to_csv('new_file.csv', encoding='UTF-8', index=False)
+
+
+main('utf8.csv')
