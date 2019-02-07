a6f66f5f06119c448454659ec4b3ac7fc377f9f0
henryy07 <adam.harmasz@o2.pl>
2019-02-07 13:58:09 +0100

    wrapping setting args in a function

--- main.py
+++ main.py
@@ -7,36 +7,37 @@ import csv
 from utils import check_encoding, input_data_manipulation
 
 
-# Creating argparse variables
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "-f",
-    help="full path of csv file you want to process",
-    type=str,
-    required=True,
-)
-parser.add_argument(
-    "-nf",
-    help="name of the file and path of the directory that will be created",
-    type=str,
-    required=True,
-)
+def set_args():
+    """Function for setting argaparse variables"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-f", "--file_path",
+        help="Specify path for the file you want to process",
+        dest='file',
+        type=str,
+        required=True,
+    )
+    parser.add_argument(
+        "-nf", "--new_file_path",
+        help="Specify name and path in which new file will be saved",
+        dest='new_file',
+        type=str,
+        required=True,
+    )
+    args = parser.parse_args()
+    return args
 
-args = parser.parse_args()
-file_path = os.path.abspath(args.f)
-new_file_path = os.path.abspath(args.nf)
 
-
-def main():
+def main(args):
     """Main function of the application"""
-    loop_control = True
+
     # checking if path of the file exist
-    if os.path.exists(file_path):
+    if os.path.exists(args.file):
         encode_type = 'UTF-8'
-        if check_encoding(file_path) == 'UTF-16':
+        if check_encoding(args.file) == 'UTF-16':
             encode_type = 'UTF-16'
 
-        with open(file_path, 'r', encoding=encode_type) as f:
+        with open(args.file, 'r', encoding=encode_type) as f:
             # create csv reader
             csv_reader = csv.DictReader(f)
             new_list = input_data_manipulation(csv_reader)
@@ -48,24 +49,23 @@ def main():
             # grouping date by date and state
             date_groups = df.groupby(
                 ['date', 'state']).sum().reset_index()
-
             # if the path for a new file doesnt exist error will be raised
             try:
                 # writing data to new csv file with enconding UTF-8
                 date_groups.to_csv(
-                    new_file_path,
+                    args.new_file,
                     encoding='UTF-8',
                     index=False,
                     line_terminator='\n'
                 )
                 sys.stdout.write(
-                    f'\nFile has been created here:\n{new_file_path}\n')
+                    f'\nFile has been created here:\n{args.new_file}\n')
             except FileNotFoundError:
                 sys.stderr.write(
-                    f'{new_file_path}-there is no such file or directory\n')
+                    f'{args.new_file} - there is no such file or directory\n')
     else:
-        sys.stderr.write(f'{file_path} - there is no such file or\n')
+        sys.stderr.write(f'{args.file} - there is no such file or\n')
 
 
 if __name__ == '__main__':
-    main()
+    main(set_args())
